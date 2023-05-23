401c6545d170983dde399f90edb143c3763f33ca
Euclid_remote <ouweijie123@outlook.com>
2023-05-24 00:45:24 +0800

    perf(优化TableInfo): 将dataYes的股票数据单做为json存储
    存储为dataYesStockData_tableInfo.json

--- Euclid_work/Quant_Share/TableInfo.py
+++ Euclid_work/Quant_Share/TableInfo.py
@@ -53,146 +53,6 @@ gmStock = {
     },
 }
 
-dataYesStock = {
-    'HKshszHold': {
-        'assets': 'stock',
-        'description': '沪深港通持股记录',
-        'date_column': 'endDate',
-        'ticker_column': 'ticker'
-    },
-    'MktEqud': {
-        'assets': 'stock',
-        'description': '沪深股票日行情',
-        'date_column': 'tradeDate',
-        'ticker_column': 'ticker'
-    },
-    'MktIdx': {
-        'assets': 'stock',
-        'description': '指数日行情',
-        'date_column': 'tradeDate',
-        'ticker_column': 'ticker',
-    },
-    'IndustryID_Sw21': {
-        'assets': 'stock',
-        'description': '申万行业分类2021',
-        'date_column': 'date',
-        'ticker_column': 'winCode'
-    },
-    'MktLimit': {
-        'assets': 'stock',
-        'description': '沪深涨跌停限制',
-        'date_column': 'tradeDate',
-        'ticker_column': 'ticker'
-    },
-    'SecST': {
-        'assets': 'stock',
-        'description': '沪深ST',
-        'date_column': 'tradeDate',
-        'ticker_column': 'ticker'
-    },
-    'SecHalt': {
-        'assets': 'stock',
-        'description': '沪深停复牌',
-        'date_column': 'haltBeginTime',
-        'ticker_column': 'ticker'
-    },
-    'ResConIndex': {
-        'assets': 'stock',
-        'description': '指数一致预期数据表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'indexCode'
-    },
-    'ResConIndexFy12': {
-        'assets': 'stock',
-        'description': '指数一致预期动态预测数据表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'indexCode'
-    },
-    'ResConIndustryCitic': {
-        'assets': 'stock',
-        'description': '中信行业一致预期数据表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'induID'
-    },
-    'ResConIndustryCiticFy12': {
-        'assets': 'stock',
-        'description': '中信行业一致预期动态预测数据表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'induID'
-    },
-    'ResConIndustrySw': {
-        'assets': 'stock',
-        'description': '申万行业一致预期数据表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'induID'
-    },
-    'ResConIndustrySwFy12': {
-        'assets': 'stock',
-        'description': '申万行业一致预期动态预测数据表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'induID'
-    },
-    'ResConSecReportHeat': {
-        'assets': 'stock',
-        'description': '个股研报热度统计数据表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'secCode'
-    },
-    'ResConSecFy12': {
-        'assets': 'stock',
-        'description': '个股一致预期动态预测数据表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'secCode'
-    },
-    'ResConSecCoredata': {
-        'assets': 'stock',
-        'description': '个股一致预期核心表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'secCode'
-    },
-    'ResConSecTarpriScore': {
-        'assets': 'stock',
-        'description': '个股一致预期目标价与评级表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'secCode'
-    },
-    'ResConSecCorederi': {
-        'assets': 'stock',
-        'description': '个股一致预期核心加工表',
-        'date_column': 'repForeDate',
-        'ticker_column': 'secCode'
-    },
-    'FdmtDerPit': {
-        'assets': 'stock',
-        'description': '财务衍生数据 (Point in time)',
-        'date_column': 'publishDate',
-        'ticker_column': 'ticker'
-    },
-    'RMExposureDay': {
-        'assets': 'stock',
-        'description': '因子暴露表',
-        'date_column': 'tradeDate',
-        'ticker_column': 'ticker'
-    },
-    'FdmtIndiRtnPit': {
-        'assets': 'stock',
-        'description': '财务指标—盈利能力 (Point in time)',
-        'date_column': 'endDate',
-        'ticker_column': 'ticker'
-    },
-    'MktEqudEval': {
-        'assets': 'stock',
-        'description': '沪深估值信息',
-        'date_column': 'tradeDate',
-        'ticker_column': 'ticker'
-    },
-    'FdmtIndiPSPit': {
-        'assets': 'stock',
-        'description': '财务指标—每股 (Point in time)',
-        'date_column': 'endDate',
-        'ticker_column': 'ticker'
-    },
-}
 gmFuture = {
     'Broker_Data': {
         'assets': 'future',
@@ -214,16 +74,18 @@ gmFuture = {
     },
 }
 
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_dir, 'dev_files/dataYesStockData_tableInfo.json'), 'r', encoding='utf-8') as f:
+    dataYesStock = json.load(f)
 # 针对D:\Share\Stk_Data\gm 下的表
 # 从gmStockFactor_tableInfo.json中读取
-current_dir = os.path.abspath(os.path.dirname(__file__))
-
 with open(os.path.join(current_dir, 'dev_files/gmStockFactor_tableInfo.json'), 'r') as f:
-    gmStockFactor = json.load(f)  # 此时a是一个字典对象
+    gmStockFactor = json.load(f)
 with open(os.path.join(current_dir, 'dev_files/gmStockData_tableInfo.json'), 'r') as f:
-    gmStockData = json.load(f)  # 此时a是一个字典对象
+    gmStockData = json.load(f)
 with open(os.path.join(current_dir, 'dev_files/emData_tableInfo.json'), 'r') as f:
-    emData = json.load(f)  # 此时a是一个字典对象
+    emData = json.load(f)
 
 tableInfo = {}
 for table in ['InfoTable', 'gmStock', 'dataYesStock', 'gmFuture', 'gmStockFactor', 'gmStockData', 'emData']:
