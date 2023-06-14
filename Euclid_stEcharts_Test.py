aa2096e8e15868d0ef9a7af76e4312c8b387e6d0
AlkaidYuan <1176671796@qq.com>
2023-06-14 18:10:30 +0800

    ICIR绘图

--- Euclid_stEcharts_Test.py
+++ Euclid_stEcharts_Test.py
@@ -6,7 +6,7 @@
 """
 import os
 from Euclid_work.Quant_Share.BackTest_Meng import *
-from Euclid_work.Quant_Share import get_tradeDate
+from Euclid_work.Quant_Share import get_tradeDate, get_data
 import streamlit as st
 from pyecharts.charts import Line
 from pyecharts.charts import Bar
@@ -22,6 +22,7 @@ import pyecharts.options as opts
 from pyecharts.charts import Line
 import streamlit as st
 import pandas as pd
+from scipy.stats import spearmanr
 
 
 def get_nav_data_2_plot(data: pd.Series):
@@ -54,31 +55,42 @@ end_date = st.sidebar.date_input('回测截至日期', value=pd.to_datetime('202
 # 研究标的
 bench_code = st.sidebar.selectbox("研究标的", ('000852.XSHG', '000905.XSHG', '000300.XSHG'))
 
-tradeDateCol = st.text_input('日期列名称', value='tradeDate')
-uploaded_file = st.file_uploader("## uploader wide score data", type=['h5','csv'])
-score = None
-if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
-    st.write("filename:", uploaded_file.name)
-    if uploaded_file.name.endswith('csv'):
-        score = pd.read_csv(uploaded_file)
-    elif uploaded_file.name.endswith('h5'):
-        # score = pd.read_hdf(uploaded_file.name)
-        score_hdf = pd.HDFStore(r'E:\Share\Stk_Data\gm\balance_sheet\balance_sheet_Y2022.h5')
-        score = score_hdf.get('/a')
-    score = score.set_index(tradeDateCol)
-    score.index = pd.to_datetime(score.index)
-    st.write(score)
+tradeDateCol_factor = st.text_input('因子日期列名称', value='tradeDate')
+uploaded_factor = st.file_uploader("## uploader wide factor data", type=['h5','csv'])
+tradeDateCol_rtn = st.text_input('回报率日期列名称', value='tradeDate')
+uploaded_rtn = st.file_uploader("## uploader wide return data", type=['h5','csv'])
 
+factor = None
+score = None
+rtn = None
+def read_file(uploaded_file, tradeDateCol):
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        st.write("filename:", uploaded_file.name)
+        if uploaded_file.name.endswith('csv'):
+            data = pd.read_csv(uploaded_file)
+            data = data.set_index(tradeDateCol)
+            return data
+        elif uploaded_file.name.endswith('h5'):
+            data = pd.HDFStore(r'E:\Share\Stk_Data\gm\balance_sheet\balance_sheet_Y2022.h5')
+            data = data.get('/a')
+            data = data.set_index(tradeDateCol)
+            return data
+        return None
+
+factor = read_file(uploaded_factor, tradeDateCol_factor)
+if factor is not None:
+    factor.index = pd.to_datetime(factor.index)
+    factor = reindex(factor)
+    score = info_lag(data2score(factor), n_lag=1)
+    st.write("因子数据前五行为：")
+    st.write(factor.head())
+
+rtn = read_file(uploaded_rtn, tradeDateCol_rtn)
 
 @st.cache_resource
 def bkTest():
-    # score prepare
-    # data = get_data('HKshszHold', begin='20180101', end='20221231')
-    # data = data.pivot(index='endDate', columns='ticker', values='partyPct')
-    # data = reindex(data.pct_change(periods=5, axis=0))
-    # score = info_lag(data2score(data), n_lag=1)  # do not use future info
-
+    global score
     # group beck test
     _DataClass = data_prepare(start_date, end_date, bench_code)
     BTClass = simpleBT(_DataClass.TICKER, _DataClass.BENCH)
@@ -99,7 +111,6 @@ if st.sidebar.button("分组回测指标"):
     outMetrics, group_res = bkTest()
     st.write("### 回测结束, 各组指标如下")
     st.dataframe(outMetrics.set_index('group'))
-
 
 def nav_plot(_group_res, _plot_begin, _plot_end):
     _plot_begin = get_tradeDate(_plot_begin, 0)
@@ -176,3 +187,62 @@ plot_end = st.sidebar.date_input('绘图结束日期', value=pd.to_datetime('202
 if st.sidebar.button("分组净值绘图"):
     outMetrics, group_res = bkTest()
     streamlit_echarts.st_pyecharts(nav_plot(group_res, plot_begin, plot_end), height="500px", width="100%")
+
+def IC_Calc(start_date, end_date):
+    global score, rtn
+    if rtn is None:
+        price_df = get_data('MktEqud', begin=start_date, end=end_date)
+        rtn = reindex(price_df.pivot(index='tradeDate', columns='ticker', values='chgPct'))
+        st.write('自动获取回报率数值')
+
+    score.dropna(how='all', axis=0, inplace=True)
+    rankIC = pd.DataFrame(np.zeros((score.shape[0], 1)), index=score.index, columns=['rankIC'])
+    for index, values in score.iterrows():
+        if index in rtn.index:
+            tmp = pd.concat([values, rtn.loc[index]], axis=1, ignore_index=True)
+            tmp.dropna(axis=0, inplace=True)
+            tmp = tmp.rank(axis=0)
+            # rank
+            res = spearmanr(tmp)
+            if res.pvalue > 0.05:
+                rankIC.loc[index] = 0
+            else:
+                rankIC.loc[index] = res.statistic
+    rankIC.dropna(axis=0, inplace=True)
+    IR = (rankIC.mean()) / rankIC.std()
+    return rankIC, IR
+
+def ICIR_plot(rankIC, IR, plot_start, plot_end):
+    plot_begin = get_tradeDate(plot_start, 0)
+    plot_end = get_tradeDate(plot_end, -1)
+    rankIC_lim = rankIC.loc[plot_begin:plot_end]
+    st.write(f"IC数据量为{len(rankIC_lim)}")
+    x_data = rankIC_lim.index.strftime("%Y%m%d").tolist()
+    bar = (
+        Bar(
+            init_opts=opts.InitOpts(
+                width='1000px',
+                height='600px',
+                theme="white"
+            )
+        )
+        .add_xaxis(xaxis_data=x_data)
+        .add_yaxis(
+            series_name="IC",
+            y_axis=list(rankIC_lim.values),
+            itemstyle_opts = opts.ItemStyleOpts(color="#00CD96"))
+        .set_global_opts(
+            title_opts={"text": f"IR={float(IR)}"},
+            brush_opts=opts.BrushOpts(),  # 设置操作图表的画笔功能
+            # toolbox_opts=opts.ToolboxOpts(),  # 设置操作图表的工具箱功能
+            yaxis_opts=opts.AxisOpts(name="IC"),
+            # 设置Y轴名称、定制化刻度单位
+            xaxis_opts=opts.AxisOpts(name="日期"),  # 设置X轴名称
+
+        )
+        )
+    return bar
+
+if st.sidebar.button("绘制因子ICIR图"):
+    rankIC, IR = IC_Calc(plot_begin, plot_end)
+    streamlit_echarts.st_pyecharts(ICIR_plot(rankIC, IR, plot_begin, plot_end), height="500px", width="100%")
