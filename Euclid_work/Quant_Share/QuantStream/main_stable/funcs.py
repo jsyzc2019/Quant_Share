15620d9884fbd709ea7311bca29a98d3e79bf118
AlkaidYuan <1176671796@qq.com>
2023-06-18 21:08:27 +0800

    划分正式版和开发版

---
+++ Euclid_work/Quant_Share/QuantStream/main_stable/funcs.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pandas as pd
+from Euclid_work.Quant_Share.BackTest_Meng import DataPrepare, simpleBT, isdate
+from Euclid_work.Quant_Share import get_tradeDate, get_data, reindex, info_lag, data2score
+import numpy as np
+from scipy.stats import spearmanr
+import os
+import tempfile
+
+def get_nav_data_2_plot(data: pd.Series):
+    return data / data.iloc[0] - 1
+
+@st.cache_resource
+def data_prepare(_start_date, _end_date, _bench_code):
+    # group beck data prepare
+    _DataClass = DataPrepare(beginDate=_start_date, endDate=_end_date, bench=_bench_code[3:6])
+    _DataClass.get_Tushare_data()
+    return _DataClass
+
+def read_file(uploaded_file, tradeDateCol='', auto=False):
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        st.write(f"正在读取：{uploaded_file.name}")
+        if uploaded_file.name.endswith('csv'):
+            data = pd.read_csv(uploaded_file)
+        elif uploaded_file.name.endswith('h5'):
+            # 创建临时文件
+            temp_file = tempfile.NamedTemporaryFile(delete=False)
+            # 将上传的文件内容保存到临时文件
+            temp_file.write(uploaded_file.read())
+            # 关闭临时文件
+            temp_file.close()
+            file_path = temp_file.name
+            data = pd.read_hdf(file_path)
+            os.remove(file_path)
+        else:
+            raise NotImplemented(uploaded_file.name.split('.')[-1])
+
+        if auto:
+            col_names = data.columns
+            for col in col_names:
+                sample = data[col].dropna().values[0]
+                if isdate(str(sample)):
+                    tradeDateCol = col
+                    break
+
+        data = data.set_index(tradeDateCol)
+        return data, tradeDateCol
+
+@st.cache_resource
+def factor2score(factor, verbose=True):
+    factor.index = pd.to_datetime(factor.index)
+    factor = reindex(factor)
+    score = info_lag(data2score(factor), n_lag=1)
+    if verbose:
+        st.write("因子数据前五行为：")
+        st.write(factor.head())
+    return factor, score
+
+@st.cache_resource
+def bkTest(_score, _start_date, _end_date, _bench_code, key=''):
+    # group beck test
+    _DataClass = data_prepare(_start_date, _end_date, _bench_code)
+    BTClass = simpleBT(_DataClass.TICKER, _DataClass.BENCH)
+    if _score is not None:
+        tmpScore = _score
+    else:
+        tmpScore = _DataClass.Score
+    _, _outMetrics, _group_res = BTClass.groupBT(tmpScore)
+    return _outMetrics, _group_res
+
+@st.cache_resource
+def IC_Calc(_rtn, _score, _start_date, _end_date):
+    if _rtn is None:
+        price_df = get_data('MktEqud', begin=_start_date, end=_end_date)
+        _rtn = reindex(price_df.pivot(index='tradeDate', columns='ticker', values='chgPct'))
+        st.write('自动获取回报率数值')
+
+    _score.dropna(how='all', axis=0, inplace=True)
+    rankIC = pd.DataFrame(np.zeros((_score.shape[0], 1)), index=_score.index, columns=['rankIC'])
+    for index, values in _score.iterrows():
+        if index in _rtn.index:
+            tmp = pd.concat([values, _rtn.loc[index]], axis=1, ignore_index=True)
+            tmp.dropna(axis=0, inplace=True)
+            tmp = tmp.rank(axis=0)
+            # rank
+            res = spearmanr(tmp)
+            if res.pvalue > 0.05:
+                rankIC.loc[index] = 0
+            else:
+                rankIC.loc[index] = res.correlation
+    rankIC.dropna(axis=0, inplace=True)
+    IR = (rankIC.mean()) / rankIC.std()
+    return rankIC, IR
---
+++ Euclid_work/Quant_Share/QuantStream/main_stable/plots.py
@@ -0,0 +1,104 @@
+from Euclid_work.Quant_Share import get_tradeDate, get_data
+from pyecharts.charts import Line
+from pyecharts.charts import Bar
+from pyecharts.charts import Scatter
+import pyecharts.options as opts
+from funcs import get_nav_data_2_plot
+import streamlit as st
+
+def nav_plot(_group_res, _plot_begin, _plot_end, _bench_code):
+    _plot_begin = get_tradeDate(_plot_begin, 0)
+    _plot_end = get_tradeDate(_plot_end, -1)
+
+    bench_nav = _group_res['4'][0] / _group_res['4'][2]
+
+    # 回测累计收益率图
+    x_data = bench_nav.loc[_plot_begin:_plot_end].index.strftime("%Y%m%d").tolist()
+    line = (
+        Line(
+            init_opts=opts.InitOpts(
+                width='1000px',
+                height='600px',
+                theme="white"
+            )
+        )
+        .add_xaxis(xaxis_data=x_data)
+        .add_yaxis(
+            series_name="group 1",
+            y_axis=get_nav_data_2_plot(_group_res['{}'.format(1)][0].loc[_plot_begin:_plot_end]).tolist(),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .add_yaxis(
+            series_name="group 2",
+            y_axis=get_nav_data_2_plot(_group_res['{}'.format(2)][0].loc[_plot_begin:_plot_end]).tolist(),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .add_yaxis(
+            series_name="group 3",
+            y_axis=get_nav_data_2_plot(_group_res['{}'.format(3)][0].loc[_plot_begin:_plot_end]).tolist(),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .add_yaxis(
+            series_name="group 4",
+            y_axis=get_nav_data_2_plot(_group_res['{}'.format(4)][0].loc[_plot_begin:_plot_end]).tolist(),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .add_yaxis(
+            series_name="group 5",
+            y_axis=get_nav_data_2_plot(_group_res['{}'.format(5)][0].loc[_plot_begin:_plot_end]).tolist(),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .add_yaxis(
+            series_name="{}".format(_bench_code),
+            y_axis=get_nav_data_2_plot(bench_nav.loc[_plot_begin:_plot_end]).tolist(),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="回测累计收益率"),  # 标题参数
+            tooltip_opts=opts.TooltipOpts(trigger="axis"),
+            yaxis_opts=opts.AxisOpts(
+                type_="value",
+                axistick_opts=opts.AxisTickOpts(is_show=True),
+                splitline_opts=opts.SplitLineOpts(is_show=True),
+            ),
+            xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),
+        )
+        .set_series_opts(
+            linestyle_opts=opts.LineStyleOpts(
+                is_show=True,
+                width=2,
+            )
+        )
+    )
+    return line
+
+def ICIR_plot(rankIC, IR, _plot_begin, _plot_end):
+    _plot_begin = get_tradeDate(_plot_begin, 0)
+    _plot_end = get_tradeDate(_plot_end, -1)
+    rankIC_lim = rankIC.loc[_plot_begin:_plot_end]
+    st.write(f"IC数据量为{len(rankIC_lim)}")
+    bar = (
+        Bar(
+            init_opts=opts.InitOpts(
+                width='1000px',
+                height='600px',
+                theme="white"
+            )
+        )
+        .add_xaxis(xaxis_data=rankIC_lim.index.strftime("%Y%m%d").tolist())
+        .add_yaxis(
+            series_name="IC",
+            y_axis=rankIC_lim['rankIC'].tolist(),
+            itemstyle_opts=opts.ItemStyleOpts(color="#00CD96"))
+        .set_global_opts(
+            title_opts={"text": f"IR={float(IR)}"},
+            brush_opts=opts.BrushOpts(),  # 设置操作图表的画笔功能
+            toolbox_opts=opts.ToolboxOpts(),  # 设置操作图表的工具箱功能
+            yaxis_opts=opts.AxisOpts(name="IC"),
+            # 设置Y轴名称、定制化刻度单位
+            xaxis_opts=opts.AxisOpts(name="日期"),  # 设置X轴名称
+
+        )
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+    )
+    return bar
